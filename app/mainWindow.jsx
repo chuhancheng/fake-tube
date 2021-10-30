90f0834a9c7194a3526b096ea9abb53876ff8209
darren <chuhancheng@gmail.com>
2021-10-30 23:54:06 +0800

    add comment

--- app/mainWindow.jsx
+++ app/mainWindow.jsx
@@ -43,6 +43,8 @@ class MainWindow extends React.Component {
         return (
             <div>
                 <HashRouter>
+                    {/* <Switch>遍歷它所有的子層<Route>，然後只渲染與它當前location匹配的第一個<Route/> */}
+                    {/* https://ithelp.ithome.com.tw/articles/10205186 */}
                     <Switch>
                         <Route path="/" exact component={Home} />
                         <Route path="/firstPage" component={FirstPage} />
